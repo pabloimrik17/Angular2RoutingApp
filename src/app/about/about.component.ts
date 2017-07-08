87d0292315633111ec4fe4e78d6112c35c6b5dfa
Etherless <pabloimrik17@gmail.com>
2017-07-08 19:08:20 +0200

    About design. User class.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../shared/models/user";
+
+const users: User[] = [
+  { id: 1, name: "Pablo", username: "pabloim",  avatar: 'https://pbs.twimg.com/profile_images/3088172495/c1afe3a1f1c6ba15aaed04873b536397_400x400.jpeg' },
+  { id: 2, name: "Lucia", username: "luor",     avatar: 'https://pbs.twimg.com/profile_images/875829059439153152/JvHgB_Un_400x400.jpg' },
+  { id: 3, name: "Jose",  username: "xelu",     avatar: 'https://pbs.twimg.com/profile_images/1783118571/adelgado2011_400x400.jpg' },
+];
 
 @Component({
   selector: 'about-page',
@@ -6,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  users: User[] = users;
   constructor() { }
 
   ngOnInit() {
